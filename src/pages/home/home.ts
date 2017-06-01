5f5924917fcabd3ec248083c99acc2f7736503b1
shinshinshaolin <shinshinshaolin@gmail.com>
2017-06-01 11:01:13 -0300

    listando usuarios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FirebaseListObservable } form 'angularfire2';
+
 import { SignupPage } from '../signup/signup';
+import { User } from './../../models/user.model';
+import { UserService } from './../../providers/user/user.service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: FirebaseListObservable<User[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  	public navCtrl: NavController,
+  	public userService: UserService
+  	) {
+  }
+
+  ionViewDidLoad() {
+  	this.users = this.userService.users;
+  }
+
+  onChatCreate(user: User): void {
+  	console.log('User: ',user);
   }
 
   onSignup(): void {
